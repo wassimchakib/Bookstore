2e8c7a96042b0dedb8a1799284ee9f62f5dfe1c2
Wassim <wassimchakib97@gmail.com>
2022-12-13 00:18:17 +0100

    Add categories reducer + configure store

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,10 +1,24 @@
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 
-const bookReducer = (state = [], actions) => {
+export const addBook = (title, author) => ({
+  type: ADD_BOOK,
+  title,
+  author,
+});
 
-};
+export const removeBOok = (title) => ({
+  type: REMOVE_BOOK,
+  title,
+});
 
-const addBook = (title, author) => {
-  
+export const bookReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, { title: action.title, author: action.author }];
+    case REMOVE_BOOK:
+      return state.filter((obj) => obj.title !== action.title);
+    default:
+      return state;
+  }
 };
